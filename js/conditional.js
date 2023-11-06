b282361ebef230376a23b57f99f34f58e946b1d4
daniel <daniel@oikarainen.se>
2023-11-06 22:07:31 +0100

    Added switch statement

--- js/conditional.js
+++ js/conditional.js
@@ -22,3 +22,47 @@ let ageOfPerson = 60;
 // else {
 //     console.log("You are a 👴[old age]");
 // }
+
+
+let monthNumber = 1;
+
+switch (monthNumber){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mars");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;    
+    case 7:
+        console.log("Juli");
+        break;    
+    case 8:
+        console.log("August");
+        break;    
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;   
+    case 11:
+        console.log("November");
+        break;  
+    case 12:
+        console.log("December");
+        break;  
+    default:
+        console.log("Invalid month")    
+}
