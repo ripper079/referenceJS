e114317917d1d9f0c75885d94a9ba217bec1c012
daniel <daniel@oikarainen.se>
2023-11-06 22:00:38 +0100

    If conditionals

---
+++ js/conditional.js
@@ -0,0 +1,24 @@
+console.log("Conditionals working");
+// if/else statements
+let funEmoji = false;
+let ageOfPerson = 60; 
+// if (funEmoji)
+// {
+//     console.log("This is funny 😀");
+// }
+// else {
+//     console.log("This is not funny 😡");
+// }
+
+// if (ageOfPerson < 2){
+//     console.log("You are a 👶[baby]");
+// }
+// else if (ageOfPerson < 18){
+//     console.log("You are a 👧[teen]");
+// }
+// else if (ageOfPerson < 55){
+//     console.log("You are a 🧔[middle aged]");
+// }
+// else {
+//     console.log("You are a 👴[old age]");
+// }
